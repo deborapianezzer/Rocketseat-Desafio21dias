6f69681d33a2d0eaf2f6a05d56b8fb9303247bdb
deborapianezzer <deboradpianezzer@gmail.com>
2022-08-19 09:26:02 -0300

    Realizando algumas interações testes.

--- pages/interações.js
+++ pages/interações.js
@@ -1,2 +1,6 @@
-var nome = window.prompt('Qualé o seu nome?')
-document.writeln(`Seu ${nome} tem ${nome.length} letras.`)
+var n2= Number(window.prompt('Digite um número:'))
+var n2= Number(window.prompt('Digite outro número:'))
+var s = n1+n2
+window.alert(´A soma entre ${n1} e ${n2} é igual a:${s}´)
+var nome = window.prompt('Qual é o seu nome?')
+document.writeln(`Seu nome ${nome} tem ${nome.length} letras.`)
